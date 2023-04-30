4b778e5d8692c1dd43b263eb554b419a3dfd0e00
archanab811 <archanabhosale7@gmail.com>
2023-04-30 09:32:59 +0530

    Swift UI setup

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Archana Mahesh Chigare on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
